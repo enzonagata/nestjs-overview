8dc91502fde146853dd25444913a76fa93bdda36
Enzo Nagata <enzo.nagata@gmail.com>
2022-09-21 21:59:36 -0300

    EOF to LF

--- src/middleware/logger.middleware.spec.ts
+++ src/middleware/logger.middleware.spec.ts
@@ -1,7 +1,7 @@
-import { LoggerMiddleware } from './logger.middleware';
+import { logger } from './logger.middleware';
 
 describe('LoggerMiddleware', () => {
   it('should be defined', () => {
-    expect(new LoggerMiddleware()).toBeDefined();
+    expect(logger).toBeDefined();
   });
 });
